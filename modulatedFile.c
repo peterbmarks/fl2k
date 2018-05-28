a287c2023497d6eaa3d7ee6a919aba2f9adb848e
Peter Marks <peter.marks@pobox.com>
2018-05-28 10:17:02 +1000

    use radians throughout

--- modulatedFile.c
+++ modulatedFile.c
@@ -5,6 +5,10 @@
 //  Created by Peter Marks on 26/5/18.
 //  Copyright © 2018 Peter Marks. All rights reserved.
 //
+// Default sample rate for fl2k_file is 100e6 or 100MHz
+// so 100e6 / 60 samples gives a carrier on 1.66MHz
+// 80 samples gives a carrier on 1.25MHz
+// 
 
 // fl2k_file samples.dat
 #include <stdio.h>
@@ -13,48 +17,51 @@
 
 const char *outFileName = "samples.dat";
 
-double radian(double degree) {
-    return degree * M_PI / 180.0;
-}
-/*
- * // produce a sine wave suitable for fl2k_file
-int main(int argc, const char * argv[]) {
+void makeCarrier(int samplesPerCycle) {
     FILE *outfile = fopen(outFileName, "wb");
     int8_t byte;
-    for(int degree = 0; degree < (360 * 1); degree++) {
-        double carrier_sin_value = sin(radian(degree % 360));
+    for(int sample = 0; sample < samplesPerCycle; sample++) {
+		double current_radian = M_PI * sample * 2 / samplesPerCycle;
+        double carrier_sin_value = sin(current_radian);
         byte = (int8_t)(carrier_sin_value * 127.0);
-        printf("%03d, val = %f, byte = %d\n", degree, carrier_sin_value, (int)byte);
+        printf("%f, val = %f, byte = %d\n", current_radian, carrier_sin_value, (int)byte);
         fwrite(&byte, sizeof(byte), 1, outfile);
     }
     fclose(outfile);
-    return 0;
 }
-*/
+
+
 
 // Produces an AM'd signal suitable for fl2k_file
-int main(int argc, const char * argv[]) {
+void makeAm(int samplesPerCycle) {
     // ratio of the carrier to the modulating sine wave
     int ratio = 100;
 
     FILE *outfile = fopen(outFileName, "wb");
+    // make sure we get enough samples for a full wave of the modulation
+	int totalSamples = samplesPerCycle * ratio;
+    for(int sample = 0; sample < totalSamples; sample++) {
+		double carrier_radian = fmod((M_PI * sample * 2 / samplesPerCycle),(M_PI * 2));
+        double mod_radian = fmod((M_PI * sample * 2 / samplesPerCycle) / ratio,(M_PI * 2));
+        
+		printf("%03d carrier r = %f, mod r = %f\n", sample, carrier_radian, mod_radian);
 
-    for(int degree = 0; degree < (360 * ratio); degree++) {
-        double carrier_degree = (double)degree / (double)ratio;
-        double mod_degree = (double)(degree % 360);
-        double carrier_sin_value = sin(radian(carrier_degree));
-        double mod_sin_value = sin(radian(mod_degree));
+        double carrier_sin_value = sin(carrier_radian);
+        double mod_sin_value = sin(mod_radian);
+        
         double am_sample = carrier_sin_value * mod_sin_value;
-        //printf("%f\t%f\t%f\n", carrier_sin_value, mod_sin_value, am_samples);
-        //printf("%f\n", am_samples);
+        //printf("%f\t%f\t%f\n", carrier_sin_value, mod_sin_value, am_sample);
 
-        // turn -1 to +1 into 0 to 255
         int8_t byte = (int8_t)(am_sample * 127.0);
-        printf("byte = %d\n", byte);
+        //printf("byte = %d\n", byte);
         fwrite(&byte, sizeof(byte), 1, outfile);
     }
     fclose(outfile);
-
-    return 0;
 }
 
+// produce a sine wave suitable for fl2k_file
+int main(int argc, const char * argv[]) {
+	//makeCarrier(80);
+	makeAm(80);
+	return 0;
+}
